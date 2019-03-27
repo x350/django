9f249b92bec8c81175306e6ef12739233b12a54e
Dmitrii Shulyak <x350sh@gmail.com>
2019-03-27 23:56:48 +0300

    update 2 format_date

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -10,7 +10,6 @@ def format_date(value):
     post_time = time.gmtime(t_v)
     y = time.strftime('%Y-%m-%d %H:%M:%S', post_time)
     format_time = '%Y-%m-%d %H:%M:%S'
-    print(y)
     date_since = datetime.strptime(y, format_time)
     c = datetime.now() - date_since
     delta_seconds = c.seconds
@@ -19,7 +18,7 @@ def format_date(value):
     if (delta_seconds < 60 * 10):
         return 'только что'
     elif (delta_days < 1):
-        return str(delta_seconds // (24 * 60 * 60)) + ' часов назад'
+        return str(delta_seconds // (60 * 60)) + ' часов назад'
     else:
         return  time.strftime('%Y-%m-%d', post_time)
 
