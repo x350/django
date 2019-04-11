f3cf9f53554c889ecfa5d36623f27c665c0f3ce8
Dmitrii Shulyak <x350sh@gmail.com>
2019-04-11 15:53:49 +0300

    update task 3

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -8,19 +8,4 @@ class StudentListView(ListView):
     ordering = 'group'
 
     def get_queryset(self):
-        # set = self.model.objects.all()
-        # queryset = {student.group: {student.teacher.all()} for student in self.model.objects.all()}
-        queryset = self.model.objects.all().prefetch_related('teacher').values('name', 'teacher__name', 'teacher__subject')
-
-        print(queryset)
-        return queryset
-
-
-
-
-# class TeacherListView(ListView):
-#     model = Teacher
-#     def get_queryset(self):
-#         queryset = self.model.objects.students
-#         print(queryset.values())
-#         return queryset
+        return self.model.objects.all().prefetch_related('teacher').values('name', 'teacher__name', 'teacher__subject')
