8a321dfe2f717f8f3ed942210e26f74eeed49ee4
Dmitrii Shulyak <x350sh@gmail.com>
2019-04-17 21:23:33 +0300

    fix task3 in dynamic-templates

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -4,6 +4,7 @@ import time
 
 register = template.Library()
 
+
 @register.filter
 def format_date(value):
     t_v = int(value)
@@ -15,13 +16,12 @@ def format_date(value):
     delta_seconds = c.seconds
     delta_days = c.days
 
-    if (delta_seconds < 60 * 10):
+    if delta_seconds < 60 * 10:
         return 'только что'
-    elif (delta_days < 1):
+    elif delta_days < 1:
         return str(delta_seconds // (60 * 60)) + ' часов назад'
     else:
-        return  time.strftime('%Y-%m-%d', post_time)
-
+        return time.strftime('%Y-%m-%d', post_time)
 
 
 # необходимо добавить фильтр для поля `score`
@@ -29,29 +29,34 @@ def format_date(value):
 def format_rating(value):
     # Ваш код
     rating = int(value)
-    if (rating < -5):
+    if rating < -5:
         return 'все плохо'
-    elif ((rating >= -5) and (rating <= 5 )):
+    elif (rating >= -5) and (rating <= 5):
         return 'нейтрально'
-    elif (rating > 5):
+    elif rating > 5:
         return 'хорошо'
     else:
         return value
-
 
 
 @register.filter
 def format_num_comments(value):
     # Ваш код
     num_comm = int(value)
-    if (num_comm == 0):
+    if num_comm == 0:
         return 'Оставте комментарий'
-    elif ((num_comm > 0) and (num_comm <= 50 )):
+    elif (num_comm > 0) and (num_comm <= 50):
         return str(num_comm)
-    elif (num_comm > 50):
+    elif num_comm > 50:
         return '50+'
     else:
         return value
 
 
-
+@register.filter
+def format_selftext(text, count):
+    if len(text) > (count * 2 + 1):
+        array_word = text.split(' ')
+        return ' ... '.join([' '.join(array_word[:count]), ' '.join(array_word[-count:])])
+    else:
+        return text
