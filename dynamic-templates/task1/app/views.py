a570ce89786af931b531ef93ab63a61cb012e324
Dmitrii Shulyak <x350sh@gmail.com>
2019-04-17 20:12:16 +0300

    fix_2 task1 in dynamic-templates

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from django.conf import settings
 import os, csv
 
+
 class InflationView(TemplateView):
     template_name = 'inflation.html'
 
@@ -17,25 +18,6 @@ class InflationView(TemplateView):
             for row in reader:
                 item = row.popitem()
                 temp = item[1].split(';')
-                prepList = temp[:1]
-                for i in temp[1:-1]:
-                    if i.isdigit():
-                        prepList.append(float(i))
-                    else:
-                        prepList.append(i)
-                prepList.append(temp[-1])
-                context['data'].append(prepList)
-                break
-            for row in reader:
-                context['data'].append(readRow(row))
+                context['data'].append(temp)
+        return render(request, self.template_name, context)
 
-                # context['data'].append((float(item) for item in (row.popitem()[1]).split(';')))
-        return render(request, self.template_name,
-                      context)
-
-def readRow(row):
-    currentList = (row.popitem()[1]).split(';')
-    prepList = currentList[:1]
-    prepList += [float(item) for item in currentList[1:-1]]
-    prepList.append(currentList[-1])
-    return prepList
