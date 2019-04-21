4a49493c9121cd4e9a36e0c4d75a98ddfb4eb102
Dmitrii Shulyak <x350sh@gmail.com>
2019-04-21 13:20:38 +0300

    ru name

--- site-form-works/car_admin/app/admin.py
+++ site-form-works/car_admin/app/admin.py
@@ -7,6 +7,9 @@ from .forms import ReviewAdminForm
 class CarAdmin(admin.ModelAdmin):
     list_display = ('brand', 'model', 'review_count')
 
+    # def get_list_display(self, request):
+
+
 class ReviewAdmin(admin.ModelAdmin):
     form = ReviewAdminForm
     list_display = ('car', 'title')
--- site-form-works/car_admin/app/models.py
+++ site-form-works/car_admin/app/models.py
@@ -2,22 +2,28 @@ from django.db import models
 
 
 class Car(models.Model):
-    brand = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50, verbose_name='Бренд')
+    model = models.CharField(max_length=50, verbose_name='Модель')
 
     def __str__(self):
         return self.brand
 
     def review_count(self):
         return Review.objects.filter(car=self).count()
-    review_count.short_description = 'REVIEW COUNT'
+    review_count.short_description = 'Количество обзоров'
+    # brand.short_description = 'Бреднд'
+    # model.short_description = 'Модель'
 
 
 
 class Review(models.Model):
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name='Машина')
+    title = models.CharField(max_length=100, verbose_name='Название')
     text = models.TextField()
+
+    # car.short_description = 'Машина'
+    # title.short_description = 'Заголовок'
+    # text.short_description = 'Текст'
 
     def __str__(self):
         return str(self.car) + ' ' + self.title
