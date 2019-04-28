7596fea64dcf8554767555667ec945f1983e2f59
Dmitrii Shulyak <x350sh@gmail.com>
2019-04-28 23:57:56 +0300

    made task auth and reg

--- site-personalization/auth_and_reg_users/auth/views.py
+++ site-personalization/auth_and_reg_users/auth/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django import forms
-from django.core.exceptions import ValidationError
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm
@@ -16,16 +13,7 @@ def home(request):
     )
 
 
-# def signup(request):
-#     return render(
-#         request,
-#         'signup.html'
-#     )
-
-
 def signup(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect('/')
     if request.method == 'GET':
         form = UserCreationForm()
         return render(request, 'signup.html', {'form': form})
@@ -43,25 +31,19 @@ def signup(request):
 
 
 def login_user(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect('/')
     if request.method == 'GET':
         form = LoginForm()
         return render(request, 'login.html', {'form': form})
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return render(request, 'login.html')
-                else:
-                    return HttpResponseRedirect('login')
-            else:
-                return HttpResponseRedirect('signup')
+            print('form is valid')
+        username = form['username'].value()
+        password = form['password'].value()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+        return render(request, 'home.html')
 
 
 def logout_user(request):
